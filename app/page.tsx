ef9ba18b87bbe86723d4670af778be31aa5a8b84
kilbha <bhargav.mech12@gmail.com>
2023-09-14 21:52:42 +0530

    Added Basic HomePage Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,83 @@
-import { BsTwitter } from "react-icons/bs";
+import { BsBell, BsBookmark, BsEnvelope, BsTwitter } from "react-icons/bs";
+import { BiHash, BiHomeCircle, BiMoney, BiUser } from "react-icons/bi";
+import { SlOptions } from "react-icons/sl";
+import React from "react";
+// import { Inter } from "next/font/google";
+import FeedCard from "@/components/FeedCard";
 
 interface TwitterSidebarButton {
   title: string;
   icon: React.ReactNode;
 }
 
+const sidebarMenuItems: TwitterSidebarButton[] = [
+  {
+    title: "Home",
+    icon: <BiHomeCircle />,
+  },
+  {
+    title: "Explore",
+    icon: <BiHash />,
+  },
+  {
+    title: "Notifications",
+    icon: <BsBell />,
+  },
+  {
+    title: "Messages",
+    icon: <BsEnvelope />,
+  },
+  {
+    title: "BookMarks",
+    icon: <BsBookmark />,
+  },
+  {
+    title: "Twitter Blue",
+    icon: <BiMoney />,
+  },
+  {
+    title: "Profile",
+    icon: <BiUser />,
+  },
+  {
+    title: "More Options",
+    icon: <SlOptions />,
+  },
+];
+
 export default function Home() {
   return (
     <div>
       <div className="grid grid-cols-12 h-screen w-screen px-56">
-        <div className="col-span-3 flex justify-start pt-8">
-          <div className="text-4xl hover:bg-gray-800 rounded-full p-2 cursor-pointer h-fit transition-all">
+        <div className="col-span-3 pt-1">
+          <div className="text-4xl w-fit hover:bg-gray-800 rounded-full p-3 cursor-pointer h-fit transition-all">
             <BsTwitter />
           </div>
+          <div className="mt-4 text-1xl font-semibold pr-4">
+            <ul>
+              {sidebarMenuItems.map((item) => (
+                <li
+                  className="flex w-fit justify-start items-center mt-2 gap-4 hover:bg-gray-800 px-3 py-2 cursor-pointer rounded-full"
+                  key={item.title}
+                >
+                  <span className="text-3xl">{item.icon}</span>
+                  <span>{item.title}</span>
+                </li>
+              ))}
+            </ul>
+            <div className="mt-5">
+              <button className="bg-[#1d9bf0] py-2 px-4 rounded-full w-full text-lg">
+                Tweet
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="col-span-6 border-r-[0.1px] border-l-[0.1px] border-white"></div>
+        <div className="col-span-5 border-r-[0.1px] border-l-[0.1px] h-screen overflow-scroll no-scrollbar border-gray-600">
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+        </div>
         <div className="col-span-3"></div>
       </div>
     </div>
---
+++ components/FeedCard/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Image from "next/image";
+import { BiMessageRounded, BiUpload } from "react-icons/bi";
+import { FaRetweet } from "react-icons/fa";
+import { AiOutlineHeart } from "react-icons/ai";
+
+const FeedCard: React.FC = () => {
+  return (
+    <div className="border border-gray-600 border-l-0 border-r-0 border-b-0 p-5 hover:bg-slate-900 transition-all cursor-pointer">
+      <div className="grid grid-cols-12 gap-3">
+        <div className="col-span-1">
+          <Image
+            src="https://avatars.githubusercontent.com/u/58323894?v=4"
+            alt="user-avatar"
+            height={50}
+            width={50}
+          />
+        </div>
+        <div className="col-span-11">
+          <h5>Killu Bhargav</h5>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been
+          </p>
+          <div className="flex justify-between mt-5 text-xl items-center p-2 w-[90%]">
+            <div>
+              <BiMessageRounded />
+            </div>
+            <div>
+              <FaRetweet />
+            </div>
+            <div>
+              <AiOutlineHeart />
+            </div>
+            <div>
+              <BiUpload />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeedCard;
